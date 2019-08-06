19383eccc80d8e03e4ea5f4c71252369de37a2fc
William Kwok <wkwok16@uw.edu>
2019-08-05 23:01:09 -0700

    Added password information

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@
 import chalk from 'chalk';
 import clear from 'clear';
 import figlet from 'figlet';
-// import path from 'path';
-// import program from 'commander';
+import path from 'path';
+import program from 'commander';
 import { MAIN_COLOR, SECONDARY_COLOR } from './constants/CLI-Constants';
 
+// Print CLI intro line
 clear();
 console.log(
 	chalk.bgHex(SECONDARY_COLOR).hex(MAIN_COLOR)(
@@ -15,5 +16,24 @@ console.log(
 	"\n",
 	chalk.bgHex(SECONDARY_COLOR).hex(MAIN_COLOR)(
 		"by Washington iGEM"
-	)
+	),
+	"\n"
 );
+
+// Get environment configs
+const igemUsername = process.env.IGEM_USERNAME;
+const igemPassword = process.env.IGEM_PASSWORD;
+if (!igemUsername || !igemPassword) {
+	console.log(chalk.bgRed("Error! Username/Password not specified. Please see the README for more instructions."));
+	process.exit(1);
+}
+
+program
+	.version("0.0.1")
+	.description("Deploy to the iGEM Site easier!")
+	.option("-n, --newpage", "Create a new page")
+	.parse(process.argv);
+
+if (program.newpage) {
+	console.log("new page is now being created");
+}
